b568bbda337dbeae38c47c1335ade6eaf5cc7e65
ShenQi1996 <qisam1989@gmail.com>
2021-04-06 12:08:21 -0400

    done with user

--- Music_App/app/models/user.rb
+++ Music_App/app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
 
+
+    validates :email, :session_token, uniqueness: true, presence: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    after_initialize :ensure_session_token
     attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return user if user && user.is_password?(password)
+    end
 
     def self.generate_session_token
         SecureRandom::urlsafe_base64
@@ -25,4 +36,6 @@ class User < ApplicationRecord
         obj = BCrypt::Password.new(self.password_digest)
         obj.is_password?(password)
     end
+
+
 end
