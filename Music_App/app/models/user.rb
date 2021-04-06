c5e6a022b587b0725023daf03a58dcba2e2b5016
syldysnya <syldysnya@gmail.com>
2021-04-06 11:41:56 -0400

    Change user.rb

--- Music_App/app/models/user.rb
+++ Music_App/app/models/user.rb
@@ -1,2 +1,28 @@
 class User < ApplicationRecord
+
+    attr_reader :password
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token #to return it
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        obj = BCrypt::Password.new(self.password_digest)
+        obj.is_password?(password)
+    end
 end
