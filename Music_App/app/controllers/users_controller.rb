88913def0f85a7e4d830aafcc646bef61992cc62
syldysnya <syldysnya@gmail.com>
2021-04-06 15:47:07 -0400

    Finish user db

--- Music_App/app/controllers/users_controller.rb
+++ Music_App/app/controllers/users_controller.rb
@@ -8,14 +8,20 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+
         if @user.save
+            login!(@user)
             redirect_to user_url(@user)
         else
+            
             render :new
         end
     end
 
-
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
 
     private
 
